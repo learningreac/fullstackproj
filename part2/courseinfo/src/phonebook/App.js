5c715118c9f06de3f19c7cbf1d31165f5c6dd8c7
learningreac <learningreact2020@gmail.com>
2021-07-26 21:35:49 -0800

    Input as a controlled component of APP

--- part2/courseinfo/src/phonebook/App.js
+++ part2/courseinfo/src/phonebook/App.js
@@ -4,21 +4,37 @@ const App = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas' }
   ]) 
-  const [ newName, setNewName ] = useState('')
+  const [ newName, setNewName ] = useState('a new name')
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    console.log('button clicked', event.target);
+    const personObj = {
+      name: newName
+    };
+
+    setPersons(persons.concat(personObj));
+    setNewName('');
+  };
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <p key={person.name}> {person.name} </p>)}
     </div>
   )
 }
