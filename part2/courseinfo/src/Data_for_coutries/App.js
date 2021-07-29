3f63f8c54166863eea1e9bb0e2cbf328a14e2dc0
learningreac <learningreact2020@gmail.com>
2021-07-29 14:40:21 -0800

    data for countries: render info when only 1 result

--- part2/courseinfo/src/Data_for_coutries/App.js
+++ part2/courseinfo/src/Data_for_coutries/App.js
@@ -1,14 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const Countries = ({input,countries}) => {
-    if(input.length>0 && countries.length > 10) {
+const Countries = ({ input, countries }) => {
+    if (input.length > 0 && countries.length > 10) {
         return (
             <p>Too many matches, specify another filter</p>
         )
+    } else if (countries.length === 1) {
+        return <Country country={countries[0]}></Country>
     }
     return (
         <div>{countries.map(country => <p key={country.name}>{country.name}</p>)}</div>
+    )
+}
+
+const Country = ({ country }) => {
+    //console.log('single country', country, country.name);
+    return (
+        <div>
+            <h1>{country.name}</h1>
+            <p>Capital:{country.capital}</p>
+            <p>Population: {country.population}</p>
+            <div>
+                <h2>Languages</h2>
+                <ul>
+                    {country.languages.map(lan => <li key={lan.name}>{lan.name}</li>)}
+                </ul>
+            </div>
+            <img src={country.flag} alt="flag" width={200} />
+        </div>
     )
 }
 
@@ -24,7 +44,7 @@ const App = () => {
             .get(`https://restcountries.eu/rest/v2/name/${input}`)
             .then(response => {
                 console.log(response.data);
-                console.log(typeof response.data)
+                //console.log(typeof response.data)
                 SetCountries(response.data);
             })
     }, [input])
@@ -35,14 +55,14 @@ const App = () => {
         SetInput(e.target.value);
     };
 
-    console.log(typeof countries);    
+    //console.log(typeof countries);    
 
     //.map(country => <p key={country.name}>{country}</p>)  // this one cause the bug
     return (
         <div>
             Find Countries:
             <input value={input} onChange={handleInputChange} />
-           <Countries countries={countries} input={input}/>
+            <Countries countries={countries} input={input} />
         </div>
     )
 };
