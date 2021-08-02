f345112f615c25747fac11938e054ee4cc3b545c
learningreac <learningreact2020@gmail.com>
2021-08-01 21:08:26 -0800

    part 3 phonebook backend 3.1-3.6

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());
+app.use(express.json());  // essential for POST middleware
 
 let notes = [
   {
@@ -54,6 +54,121 @@ app.post('/api/notes', (request, response) => {
   console.log(note)
   response.json(note) 
 });
+
+app.post('./api/notes', (request, response) => {
+  
+    const person = request.body;
+    console.log(person);
+
+    response.json(person) 
+})
+
+let persons = [
+  { 
+    id: 1,
+    name: "Arto Hellas", 
+    number: "040-123456"
+  },
+  { 
+    id: 2,
+    name: "Ada Lovelace", 
+    number: "39-44-5323523"
+  },
+  { 
+    id: 3,
+    name: "Dan Abramov", 
+    number: "12-43-234345"
+  },
+  { 
+    id: 4,
+    name: "Mary Poppendieck", 
+    number: "39-23-6423122"
+  }
+];
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+});
+
+app.get('/info', (request, response) => {
+  let size = persons.length;
+  response.send(`<p>Phonebook has info for ${size} people</p>
+                  <p>${new Date()}</p>`)
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+  if(person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+ });
+
+/*
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => p.id))
+    :0
+
+  body.id = maxId + 1
+  persons = persons.concat(body)
+  response.json(body) 
+})
+*/
+
+
+
+  const generateId = () => {
+    const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => p.id))
+    :0
+
+    return maxId + 1
+  }  
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+
+  if(!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  } else if(persons.some(n => n.name ===body.name)) {
+    return response.json({
+      error: 'name must be unique'
+    })
+  };
+
+  const person = {
+    name:body.name,
+    number:body.number,
+    date: new Date(),
+    id: generateId()
+  }
+
+  console.log(person)
+
+  persons = persons.concat(person)
+
+  response.json(person) 
+});
+
+
+//app.post('./api/persons', (request, response) => { 
+// the dot cause the former bug!!!!
 
 const PORT = 3001;
 app.listen(PORT, () => {
