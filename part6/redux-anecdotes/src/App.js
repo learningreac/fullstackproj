c79d92a22f9325ef1026a43b9468c54c541def7a
learningreac <learningreact2020@gmail.com>
2021-08-07 11:04:53 -0800

    seperate AncedoteList component

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,33 +1,12 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { voteFor } from './reducers/anecdoteReducer';
+import AnecdoteList from './components/AnecdoteList';
 import AnecdoteForm from './components/AnecdoteForm';
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  console.log('current state', anecdotes)
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    console.log('vote', id)
-    dispatch(voteFor(id));
-  };
-
-  
+ 
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList />
       <AnecdoteForm />      
     </div>
   )
