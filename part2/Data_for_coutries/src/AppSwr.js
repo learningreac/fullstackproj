1053f6efc502a1773867d3fcd0ffc8e6348f4184
learningreac <learningreact2020@gmai.com>
2022-07-01 13:52:42 -0700

    refine child component

--- part2/Data_for_coutries/src/AppSwr.js
+++ part2/Data_for_coutries/src/AppSwr.js
@@ -9,7 +9,7 @@ import CountryInfoService from './services/countrieInfo';
 const baseUrl = 'https://restcountries.com/v3.1/name';
 const AppSwr = () => {
     const [input, SetInput] = useState('');
-    const [singleCountry, setSingleCoutry] = useState(null);  // undefined will still not cause conditional rendering
+    const [singleCountry, setSingleCountry] = useState(null);  // undefined will still not cause conditional rendering
     const { data: countrydata, error } = useSWR(input ? `${baseUrl}/${input}` : null, CountryInfoService.fetcher);
     console.log("data", countrydata, error);
 
@@ -18,12 +18,6 @@ const AppSwr = () => {
         SetInput(e.target.value);
     };
 
-    const handleShowInfo = (name) => {
-        let target = countrydata.find(country => country.name.common === name);
-        setSingleCoutry(target);
-    };
-    console.log('target', singleCountry);
-
     return (
         <div className='container'>
             <h1 className='title text-center m-5'> Search for Countries and Weather</h1>
@@ -31,7 +25,7 @@ const AppSwr = () => {
                 <div className='left'>
                     <h2> Find Countries:</h2>
                     <input value={input} onChange={handleInputChange} />
-                    {countrydata && <Countries countries={countrydata} input={input} showInfo={handleShowInfo} />}
+                    {countrydata && <Countries countries={countrydata} input={input} setSingleCountry={setSingleCountry} />}
                 </div>
                 <div className='right'>
                 {singleCountry && <Country country={singleCountry}></Country>}
--- part2/Data_for_coutries/src/components/Countries.jsx
+++ part2/Data_for_coutries/src/components/Countries.jsx
@@ -1,23 +1,27 @@
 import Country from "./Country";
 
-const Countries = ({ input, countries, showInfo }) => {
+const Countries = ({ input, countries,  setSingleCountry }) => {
     
 
     if (input.length > 0 && countries.length > 10) {
-      
         return (
             <p>Too many matches, specify another filter</p>
         )
     } else if (countries.length === 1) {
-        return <Country country={countries[0]}></Country>
+        setSingleCountry(countries[0])
     }
+
+    const handleShowInfo = (name) => {
+        let target = countries.find(country => country.name.common === name);
+        setSingleCountry(target);
+    };
 
     return (
         <div>
             <ul className="">
                 {countries.map(country =>
                     <li key={country.name.common}>{country.name.common}
-                        <button className="btn btn-primary btn-sm" onClick={() => showInfo(country.name.common)}>Show</button>
+                        <button className="btn btn-primary btn-sm" onClick={() => handleShowInfo(country.name.common)}>Show</button>
                     </li>
                 )}
             </ul>
